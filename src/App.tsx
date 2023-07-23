4b6c8c9604af079f4c76976bdd374b2b48c14d64
Влад Браун <vladbraun@MacBook-Air-Vlad.local>
2023-07-23 21:53:29 +0300

    (update)add controlled input & wrap ajax into func

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,34 @@
 import "./globals.scss";
 import Header from "./Header";
-import Main from "./components/Main/Main"
+import Main from "./components/Main/Main";
 import Footer from "./Footer";
 import axios from "axios";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
-  // axios.get(' https://api.edamam.com/api/recipes/v2', {
-  //   params: {
-  //     app_id: "06af1388",
-  //     app_key: "73b44e63ba2ecf3b98e0c3a5d387e558",
-  //     health: "kosher",
-  //     type: "public"
-  //   }
-  // })
-  // .then(function (response) {
-  //   console.log(response);
-  // })
-  // .catch(function (error) {
-  //   console.log(error);
-  // })
-  // .finally(function () {
-  //   // always executed
-  // });  
+  async function getData(props: {q: String}) {
+    const response = await axios.get(" https://api.edamam.com/api/recipes/v2", {
+      params: {
+        app_id: "06af1388",
+        app_key: "73b44e63ba2ecf3b98e0c3a5d387e558",
+        ...props,
+        type: "public",
+      },
+    });
+    console.log(response)
+    return response
+  }
+  
+
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Main />,
     },
     {
-      path:"/helo",
-      element: <Footer />
-    }
+      path: "/helo",
+      element: <Footer />,
+    },
   ]);
   return (
     <>
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,22 @@
-import './globals.scss';
+import "./globals.scss";
+import { useState } from "react";
 
 function Header() {
+  const [inputValue, setInputValue] = useState("");
   return (
-    <header className='mainHeader'>
-        <div id="headerLogo">YourKitchen</div>
+    <header className="mainHeader">
+      <div id="headerLogo">YourKitchen</div>
+      <form action="">
+        <input
+          value={inputValue}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+          type="text"
+          placeholder="Find dish by the name"
+        />
+        <button id="searchBtn" type="button"></button>
+      </form>
     </header>
   );
 }
