55245b1477748193cb263ab55437ab3cf855d987
Влад Браун <vladbraun@MacBook-Air-Vlad.local>
2023-08-06 15:37:58 +0300

    (update)add dual slider

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,24 +1,15 @@
 import "./main.scss";
-import optionsImg from "../../images/options.svg"
-import searchImg from "../../images/search.svg"
-import { Link } from "react-router-dom";
+import MultiRangeSlider from "../MultiRangeSlider/MultirangeSlider";
 
 function Main() {
   return (
-    <main>
-        <h1>Pick the option you like:</h1>
-        <div id="main__buttonsWrapper">
-            <Link to={'helo'}>
-              <button type="button" id="main__optionsBtn">
-                  <img src={optionsImg} alt="" />
-                  <h3>Find something you'd like to cook</h3>
-              </button>
-            </Link>
-            <button type="button" id="main__searchBtn">
-                <img src={searchImg} alt="" />
-                <h3>Get a reciept by the dish name </h3>
-            </button>
-        </div>
+    <main id="selectorMain">
+      <h1>Don't know what to cook?</h1>
+      <div>Set up preferences and find the dishes by options</div>
+      <form id="dishOptions">
+        <h2>How many calories?</h2>
+        <MultiRangeSlider min={0} max={1000} />
+      </form>
     </main>
   );
 }
